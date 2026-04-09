6218fecf0e90f1ddf4a441bb651baa3900fb63a5
thegamingdreams <mukhamedovabdumalik@gmail.com>
2026-04-08 19:11:05 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
