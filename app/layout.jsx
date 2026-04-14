dfefdb67d0e1d606e5af991ec64f0440dd404dc6
thegamingdreams <mukhamedovabdumalik@gmail.com>
2026-04-13 21:07:23 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,11 @@ export const metadata = {
   description: "Honest roofing work with fair prices.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
